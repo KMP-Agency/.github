56e3c961cafb92bbab50f1cf361b17e4bd8a5a88
Martin PAUCOT <mpaucot@kmp.agency>
2021-05-17 11:49:06 +0200

    setup ci

--- actions/environment-parser/index.js
+++ actions/environment-parser/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const yaml = require('js-yaml');
+const _ = require('lodash');
 
 try {
     let value = core.getInput('value');
@@ -9,16 +10,22 @@ try {
     let branch = github.context.ref.split('/');
     branch = branch[branch.length - 1];
 
+    let output = {};
+
+    if (!!value.default) {
+        output = _.merge(output, value.default);
+    }
+
     for (const key of Object.keys(value)) {
         const regex = key.replace('*', '.*')
         if (!(new RegExp(regex).test(branch))) {
             continue;
         }
 
-        core.setOutput('variables', JSON.stringify(value[key]))
-        break;
+        output = _.merge(output, value[key])
     }
 
+    core.setOutput('variables', output);
 } catch (error) {
     core.setFailed(error);
 }
