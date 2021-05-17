8a079c44e3a6fa40d77402210138ff1fce1495fb
Martin PAUCOT <mpaucot@kmp.agency>
2021-05-17 11:19:36 +0200

    setup ci

--- actions/terraform/entrypoint.sh
+++ actions/terraform/entrypoint.sh
@@ -1,30 +1,6 @@
 #!/bin/sh -l
 
-PERSONNAL_API_TOKEN=$1
-DOCKERFILE=$2
-CONTEXT=$3
-USE_COMMIT_HASH=$4
+VALUES=$1
+TERRAFORM_DIRECTORY=$2
 
-echo "$PERSONNAL_API_TOKEN" | docker login ghcr.io -u $GITHUB_ACTOR --password-stdin
-
-IMAGE_ID=ghcr.io/$GITHUB_REPOSITORY
-
-# Change all uppercase to lowercase
-IMAGE_ID=$(echo $IMAGE_ID | tr '[A-Z]' '[a-z]')
-
-if [ $USE_COMMIT_HASH = '0' ]
-then
-  # Strip git ref prefix from version
-  VERSION=$(echo "$GITHUB_REF" | sed -e 's,.*/\(.*\),\1,')
-
-  # Strip "v" prefix from tag name
-  [[ "$GITHUB_REF" == "refs/tags/"* ]] && VERSION=$(echo $VERSION | sed -e 's/^v//')
-
-  # Use Docker `latest` tag convention
-  [ "$VERSION" == "master" ] && VERSION=latest
-else
-  VERSION=$(echo $GITHUB_SHA | cut -c1-8)
-fi
-
-docker build $CONTEXT --file $DOCKERFILE --tag $IMAGE_ID:$VERSION
-docker push $IMAGE_ID:$VERSION
+echo VALUES
