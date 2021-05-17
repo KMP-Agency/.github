a619d459a56ce279e2493c2325591d39c0319ce9
Martin PAUCOT <mpaucot@kmp.agency>
2021-05-17 11:46:40 +0200

    setup ci

--- actions/environment-parser/index.js
+++ actions/environment-parser/index.js
@@ -5,7 +5,20 @@ const yaml = require('js-yaml');
 try {
     let value = core.getInput('value');
     value = yaml.load(value, 'utf8');
-    console.log(github.context.ref);
+
+    let branch = github.context.ref.split('/');
+    branch = branch[branch.length - 1];
+
+    for (const key of Object.keys(value)) {
+        const regex = key.replace('*', '.*')
+        if (!(new RegExp(regex).test(branch))) {
+            continue;
+        }
+
+        core.setOutput('variables', JSON.stringify(value[key]))
+        break;
+    }
+
 } catch (error) {
     core.setFailed(error);
 }
