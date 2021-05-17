d00cdafc4fcb4a6ce9fd9abc63ee33e411a98343
Martin PAUCOT <mpaucot@kmp.agency>
2021-05-17 11:38:24 +0200

    setup ci

--- actions/environment-parser/index.js
+++ actions/environment-parser/index.js
@@ -5,7 +5,7 @@ const yaml = require('js-yaml');
 try {
     let value = core.getInput('value');
     value = yaml.load(value, 'utf8');
-    console.log(value);
+    console.log(github.context.ref);
 } catch (error) {
     core.setFailed(error);
 }
